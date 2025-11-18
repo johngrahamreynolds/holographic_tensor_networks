629b96d87035941f033b6f8b110c7261ab394758
John Graham Reynolds <johngrahamreynolds@gmail.com>
2025-11-17 18:28:52 -0600

    mps __init__ update

--- src/tensor_networks/mps/__init__.py
+++ src/tensor_networks/mps/__init__.py
@@ -1,5 +1,11 @@
+# Matrix Product States (MPS)
+# Author: John Graham Reynolds
+# *************************************************************
+
 from .mps import MPS
+from .train_mps import train_mps
 
 __all__ = [
     'MPS',
+    'train_mps',
 ]
